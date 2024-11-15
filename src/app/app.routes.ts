853c458a4d2fcc5bd4dff03ab4db4cfc2f43a00a
Danida <jayakodydanida@gmail.com>
2024-11-15 20:26:05 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {BookingComponent} from './booking/booking.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {path:'book',component:BookingComponent},
+  {path:'started',component:BookingComponent},
+  {path:'',component:HomeComponent},
+];
